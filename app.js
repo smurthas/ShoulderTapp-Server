112cce32c26398a89232bcd353835177d1ad762f
Simon Murtha-Smith <simon@murtha-smith.com>
2012-05-02 16:06:30 -0700

    alive

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express = require('express');
 var connect = require('connect');
 
 var endpoints = require('./lib/endpoints');
+var config = require('./lib/config');
 
 var app = express.createServer(connect.bodyParser());
 
 app.post('/close_friend', endpoints.addCloseFriend);
 app.delete('/close_friend', endpoints.deleteCloseFriend);
 
-endpoints.init(function() {
-  app.listen(process.env.PORT || 8080);
+app.get('/auth_start', endpoints.authStart);
+app.get('/auth_callback', endpoints.authCallback);
+
+endpoints.init(function(err) {
+  if (err) return console.error(err);
+  app.listen(config.app.port);
+  console.log('listening on ' + config.app.port);
 });
+
